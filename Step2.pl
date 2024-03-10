df419e570da52405a3765f33debeae1d95cffa37
Ybellocq <134070832+Ybellocq@users.noreply.github.com>
2024-03-10 14:56:57 +0100

    ca prend pas le saut de ligne de merde

--- Step2.pl
+++ Step2.pl
@@ -1,5 +1,3 @@
-
-
 maisons([
     maison(_,_,_,_,_) ,
     maison(_,_,_,_,_) ,
@@ -7,77 +5,62 @@ maisons([
     maison(_,_,_,_,_) ,
     maison(_,_,_,_,_)
 ]).
-
-% maison(couleur, nationalité, boisson, cigare, animal).
-
+% maison(couleur, nationalite, boisson, cigare, animal).
 maison(rouge, anglais, _, _, _).
-maison(_, suédois, _, _, chien).
-maison(_, danois, thé, _, _).
-maison(verte, _, café, _, _).
+maison(_, suedois, _, _, chien).
+maison(_, danois, the, _, _).
+maison(verte, _, cafe, _, _).
 maison(_, _, _, pall_mall, oiseaux).
 maison(jaune, _, _, dunhill, _).
 maison(_, _, lait, _, _).
-maison(_, norvégien, _, _, _).
+maison(_, norvegien, _, _, _).
 maison(_, _, _, blend, chats).
 maison(_, _, _, dunhill, cheval).
-maison(_, _, bière, blue_master, _).
+maison(_, _, biere, blue_master, _).
 maison(_, allemand, _, prince, _).
 maison(bleue, _, _, _, _).
 maison(_, _, eau, _, _).
 maison(_, _, poisson, _, _).
-
 afficher_liste([]).
 afficher_liste([Maison|Reste]) :- writeln(Maison), afficher_liste(Reste).
-
-appartient_à(X,[X|_]).
-appartient_à(X,[_|L]) :- appartient_à(X,L).
-
-
-est_à_gauche_de(A, B, [A, B|_]).
-est_à_gauche_de(A, B, [_|Y]) :- est_à_gauche_de(A, B, Y).
-
-
-est_à_côté_de(A, B, [A, B|_]).
-est_à_côté_de(A, B, [B, A|_]).
-est_à_côté_de(A, B, [_|Y]) :- est_à_côté_de(A, B, Y).
-
-
+appartient_a(X,[X|_]).
+appartient_a(X,[_|L]) :- appartient_a(X,L).
+est_a_gauche_de(A, B, [A, B|_]).
+est_a_gauche_de(A, B, [_|Y]) :- est_a_gauche_de(A, B, Y).
+est_a_côte_de(A, B, [A, B|_]).
+est_a_côte_de(A, B, [B, A|_]).
+est_a_côte_de(A, B, [_|Y]) :- est_a_côte_de(A, B, Y).
 resoudre :-
     maisons(MAISONS),
-    appartient_à(maison(rouge, anglais, _, _, _), MAISONS),
-    appartient_à(maison(_, suédois, _, _, chien), MAISONS),
-    appartient_à(maison(_, danois, thé, _, _), MAISONS),
-    est_à_gauche_de(maison(verte, _, café, _, _), maison(blanche, _, _, _, _), MAISONS),
-    appartient_à(maison(verte, _, café, _, _), MAISONS),
-    appartient_à(maison(_, _, _, pall_mall, oiseaux), MAISONS),
-    appartient_à(maison(jaune, _, _, dunhill, _), MAISONS),
+    appartient_a(maison(rouge, anglais, _, _, _), MAISONS),
+    appartient_a(maison(_, suedois, _, _, chien), MAISONS),
+    appartient_a(maison(_, danois, the, _, _), MAISONS),
+    est_a_gauche_de(maison(verte, _, cafe, _, _), maison(blanche, _, _, _, _), MAISONS),
+    appartient_a(maison(verte, _, cafe, _, _), MAISONS),
+    appartient_a(maison(_, _, _, pall_mall, oiseaux), MAISONS),
+    appartient_a(maison(jaune, _, _, dunhill, _), MAISONS),
     MAISONS = [_, _, maison(_, _, lait, _, _), _, _],
-    MAISONS = [maison(_, norvégien, _, _, _)|_],
-    est_à_côté_de(maison(_, _, _, blend, _), maison(_, _, chats, _, _), MAISONS),
-    est_à_côté_de(maison(_, _, dunhill, _, cheval), maison(_, _, _, _, dunhill), MAISONS),
-    appartient_à(maison(_, _, bière, blue_master, _), MAISONS),
-    appartient_à(maison(_, allemand, _, prince, _), MAISONS),
-    est_à_côté_de(maison(_, norvégien, _, _, _), maison(bleue, _, _, _, _), MAISONS),
-    est_à_côté_de(maison(_, _, _, blend, _), maison(_, _, _, eau, _), MAISONS),
-
-    appartient_à(maison(_, _, poisson, _, _), MAISONS),
-
+    MAISONS = [maison(_, norvegien, _, _, _)|_],
+    est_a_côte_de(maison(_, _, _, blend, _), maison(_, _, chats, _, _), MAISONS),
+    est_a_côte_de(maison(_, _, dunhill, _, cheval), maison(_, _, _, _, dunhill), MAISONS),
+    appartient_a(maison(_, _, biere, blue_master, _), MAISONS),
+    appartient_a(maison(_, allemand, _, prince, _), MAISONS),
+    est_a_côte_de(maison(_, norvegien, _, _, _), maison(bleue, _, _, _, _), MAISONS),
+    est_a_côte_de(maison(_, _, _, blend, _), maison(_, _, _, eau, _), MAISONS),
+    appartient_a(maison(_, _, poisson, _, _), MAISONS),
     afficher_liste(MAISONS).
-
-
-
 %- L’Anglais vit dans la maison rouge.
-%- Le Suédois a des chiens.
-%- Le Danois boit du thé.
-%- La maison verte est à gauche de la maison blanche.
-%- Le propriétaire de la maison verte boit du café.
+%- Le Suedois a des chiens.
+%- Le Danois boit du the.
+%- La maison verte est a gauche de la maison blanche.
+%- Le proprietaire de la maison verte boit du cafe.
 %- La personne qui fume des Pall Mall a des oiseaux.
-%- Le propriétaire de la maison jaune fume des Dunhill.
+%- Le proprietaire de la maison jaune fume des Dunhill.
 %- La personne qui vit dans la maison du centre boit du lait.
-%- Le Norvégien habite dans la première maison.
-%- L’homme qui fume des Blend vit à côté de celui qui a des chats.
+%- Le Norvegien habite dans la première maison.
+%- L’homme qui fume des Blend vit a côte de celui qui a des chats.
 %- L’homme qui a un cheval est le voisin de celui qui fume des Dunhill.
-%- Le propriétaire qui fume des Blue Master boit de la bière.
+%- Le proprietaire qui fume des Blue Master boit de la bière.
 %- L’Allemand fume des prince.
-%- Le Norvégien vit juste à côté de la maison bleue.
+%- Le Norvegien vit juste a côte de la maison bleue.
 %- L’homme qui fume des Blend a un voisin qui boit de l’eau.
