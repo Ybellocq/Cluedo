f10ebb181e076cc76b4a1df16c06d0d1690018ca
Ybellocq <134070832+Ybellocq@users.noreply.github.com>
2024-03-10 20:14:57 +0100

    step 3

--- step3.pl
+++ step3.pl
@@ -1,21 +1,29 @@
-switch(gauche, droite).
-switch(droite, gauche).
-deplacement([X, Loup, X, Chou], chevre,[Y, Loup, Y, Chou]) :- switch(X, Y).
-deplacement([X, X, Chevre, Chou], loup, [Y, Y, Chevre, Chou]) :- switch(X, Y).
-deplacement([X, Loup, Chevre, X], chou,[Y, Loup, Chevre, Y]) :- switch(X, Y).
-deplacement([X, Loup, Chevre, Chou], peon_seul, [Y, Loup, Chevre, Chou]) :-
-	switch(X, Y).
+%rive droite
+rivedroite([PEON ,LOUP, CHOU, CHEVRE]).
+%rive gauche
+rivegauche([SANSPEON, SANSLOUP, SANSCHOU, SANSCHEVRE]).
+% changement
+cross(A, B).
+cross(B, A).
+% move
+move(rivedroite, 1, rivegauche) :-  cross(PEON, SANSPEON),
+                                    cross(LOUP, SANSLOUP).
+move(rivedroite, 2, rivegauche) :-  cross(PEON, SANSPEON),
+                                    cross(CHOU, SANSCHOU).
+move(rivedroite, 3, rivegauche) :-  cross(PEON, SANSPEON),
+                                    cross(CHEVRE, SANSCHEVRE).
+move(rivedroite, 4, rivegauche) :-  cross(PEON, SANSPEON).
+% Verification de possiblibite
 tests(X, X, _).
 tests(X, _, X).
-viable([Peon, Loup, Chevre, Chou]) :- tests(Peon, Chevre, Loup),
-	tests(Peon, Chevre, Chou).
+viable([PEON, LOUP, CHEVRE, CHOU]) :-   tests(PEON, CHEVRE, LOUP),
+                                        tests(PEON, CHEVRE, CHOU).
 
-etapes([droite, droite, droite, droite], []).
-etapes(Berge, [Deplacement | ProchainDeplacements]) :-
-	deplacement(Berge, Deplacement, ProchainMouvement),
-	viable(ProchainMouvement),
-	etapes(ProchainMouvement, ProchainDeplacements).
 
-solution :- length(Deplacements,7),
-	etapes([gauche, gauche, gauche, gauche], Deplacements),
-	write(Deplacements), !.
+
+
+
+
+
+
+
